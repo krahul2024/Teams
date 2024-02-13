627f8de07ad21289c7f38c445cec13942b5f596b
Sah <rasah@softwareag.com>
2024-02-13 16:59:00 +0530

    fixed the outside click issue in filtered users list

--- Basic/src/app/components/modal-form/modal-form.component.ts
+++ Basic/src/app/components/modal-form/modal-form.component.ts
@@ -1,29 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild, numberAttribute } from '@angular/core';
 import { users as Users } from '../../../assets/data'; // @ts-ignore 
 import { User } from 'src/app/interfaces/user.ts';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-modal-form',
   templateUrl: './modal-form.component.html',
   styleUrls: ['./modal-form.component.css']
 })
-export class ModalFormComponent {
+export class ModalFormComponent implements OnInit {
+
+  @ViewChild('modalBody') modalRef! : ElementRef; 
+  @ViewChild('modalOpen') modalOpenButtonRef! : ElementRef; 
+
   showModal: boolean = true;
   enableShare: boolean = false;
   cancelSharing: boolean = false;
   includeAttachments: boolean = false;
-  // attachments: string[] = [
-  //   'Document1.pdf',
-  //   'Image.jpg',
-  //   'Presentation.pptx',
-  //   'Spreadsheet.xlsx',
-  //   'Video.mp4',
-  //   'LongFileNameThatExceedsFiftyCharactersAndShouldBeTruncated.pdf',
-  //   'AnotherLongFileNameThatExceedsFiftyCharactersAndShouldBeTruncated.pptx',
-  //   'Report.docx',
-  //   'AudioFileWithAVeryLongNameThatExceedsFiftyCharacters.mp3',
-  //   'ImportantDocumentWithAVeryLongNameThatExceedsFiftyCharacters.doc'
-  // ];
 
   attachments: string[] = [];
 
@@ -33,21 +26,22 @@ export class ModalFormComponent {
   users: User[] = Users.slice(0, 20); // hard-coded list of users
 
   filteredUsers: User[] = [];
-  selectedUsers: User[] = this.users.slice(0,7); 
+  selectedUsers: User[] = [];
   isTextareaFocused: boolean = false;
+  focusedOnFilteredList = false ; 
 
+  constructor(private elementRef: ElementRef){} 
 
-  /*
-    Users are filtered on the following basis 
-    - if the search value is included in name : displayName
-  */
+  ngOnInit(): void {
+      
+  }
 
   filterUser(): void {
     this.filteredUsers = this.users.filter((user: User) => {
       const isValidTerm = this.searchTerm.length > 0;
       const userContainsTerm = user.displayName?.toLocaleLowerCase().includes(this.searchTerm);
-      const alreadySelected = this.selectedUsers.some(selUser => selUser.displayName === user.displayName); 
-      return isValidTerm && userContainsTerm && !alreadySelected; 
+      const alreadySelected = this.selectedUsers.some(selUser => selUser.displayName === user.displayName);
+      return isValidTerm && userContainsTerm && !alreadySelected;
     });
 
     // this.filteredUsers.map(user => console.log(user.displayName))
@@ -55,11 +49,32 @@ export class ModalFormComponent {
 
   //--------Add the bottom border on focus 
   onTextareaFocus() {
+    console.log('Inside the focused area')
     this.isTextareaFocused = true;
   }
 
   onTextareaBlur() {
+    console.log('Outside of the focus')
     this.isTextareaFocused = false;
+    // if(!this.focusedOnFilteredList)this.searchTerm = ""; 
+  }
+
+  onMouseEnter(){
+    console.log('Mouse entered')
+    this.focusedOnFilteredList = true ; 
+  }
+
+  onMouseLeave(){
+    this.focusedOnFilteredList = false ; 
+    console.log('Mouse left the area')
+  }
+
+  shareWithDivFocus(){
+    console.log('share with div focused')
+  }
+
+  shareWithDivBlur(){
+    console.log('Share with div blurred')
   }
 
   //---------------Modal operations 
@@ -71,35 +86,46 @@ export class ModalFormComponent {
     this.includeAttachments = false;
     this.searchTerm = "";
     this.usersConcatenatedList = "";
-    this.filteredUsers = []; 
-    this.selectedUsers = []; 
+    this.filteredUsers = [];
+    this.selectedUsers = [];
   }
+
+  @HostListener('document : click', ['$event'])
+  onClick(event: MouseEvent) {
+    // if(this.modalOpenButtonRef.nativeElement.contains(event.target)) console.log('Clicked from the button')
+    // if(!this.modalRef.nativeElement.contains(event.target)) {
+    //   console.log('from outside-click')
+    //   this.toggleModal()
+    // } 
+  }
+
+
 
   getSelectedUser(user: User) {
     if (user) {
-      console.log(user);
+      // console.log(user);
       this.selectedUsers.push(user);
       this.usersConcatenatedList += (user.displayName + ", ");
     }
     // console.log(this.selectedUsers)
-    console.log(this.usersConcatenatedList)
+    // console.log(this.usersConcatenatedList)
     this.filteredUsers = [];
     this.searchTerm = "";
   }
 
 
   //------------Remove the selected user 
-  removeSelectedUser(user : User) {
-    this.selectedUsers = this.selectedUsers.filter((selUser : User) => {
-      return user.displayName !== selUser.displayName; 
+  removeSelectedUser(user: User) {
+    this.selectedUsers = this.selectedUsers.filter((selUser: User) => {
+      return user.displayName !== selUser.displayName;
     })
   }
 
   // ---------------- Calculate grid sizes for each item 
-  getGridTemplate(){
-    const gridTemplateCols = this.selectedUsers.map(user => `${(user.displayName || '').length * 7}px`).join(' '); 
+  getGridTemplate() {
+    const gridTemplateCols = this.selectedUsers.map(user => `${(user.displayName || '').length * 7}px`).join(' ');
     console.log(gridTemplateCols)
-    return gridTemplateCols ; 
+    return gridTemplateCols;
   }
 
 
@@ -126,12 +152,13 @@ export class ModalFormComponent {
     this.attachments = this.attachments.filter((attachment: string) => {
       return attachment !== filename;
     })
+    if (this.attachments.length == 0) this.includeAttachments = false;
   }
 
-  truncateFilename(filename: string, param : string) {
+  truncateFilename(filename: string, param: string) {
     const names = filename.split(param);
-    if(param == '.') return names[0].slice(0, 13) + '...' + names[names.length - 1];
-    else return names[0] + (names.length > 1 ? ', ' + names[names.length-1] : '').slice(0, 20); 
+    if (param == '.') return names[0].slice(0, 13) + '...' + names[names.length - 1];
+    else return names[0] + (names.length > 1 ? ', ' + names[names.length - 1] : '').slice(0, 20);
   }
 
   isValidUser(user: User): boolean {
@@ -141,12 +168,12 @@ export class ModalFormComponent {
   }
 
   selectUser(user: User) {
-    console.log(user); 
+    console.log(user);
   }
 
-  removeUser(user : User){
-    this.selectedUsers = this.selectedUsers.filter((selUser : User) => {
-      selUser.displayName !== user.displayName; 
+  removeUser(user: User) {
+    this.selectedUsers = this.selectedUsers.filter((selUser: User) => {
+      selUser.displayName !== user.displayName;
     })
   }
 
@@ -163,4 +190,8 @@ export class ModalFormComponent {
     if (user && user.userPrincipalName) return user.userPrincipalName.split('@')[0].toUpperCase();
     return "";
   }
+
+  //---------------Share the message to teams
+
+  
 }
