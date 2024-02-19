37a71df238c339adbbcf1530cffc17fda01ef7ee
Sah <rasah@softwareag.com>
2024-02-19 15:11:03 +0530

    some changes in the yaml file and added the python script

---
+++ .github/git_event_json.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+
+event_payload = os.environ.get('GITHUB_EVENT_PATH')
+
+if event_payload:
+    with open(event_payload, 'r') as f:
+        event_data = json.load(f)
+
+    variables = {
+        "repository": event_data.get('repository', {}).get('full_name'),
+        "sender": event_data.get('sender', {}).get('login'),
+    }
+
+    
+    with open('event_data.json', 'w') as f:
+        json.dump(variables, f, indent=4)
+else:
+    print("No GitHub event payload found.")
